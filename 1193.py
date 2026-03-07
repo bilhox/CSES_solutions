47c6b2eddeb2c9f2b80a1dbcfead43d4f640652f
bilhox <lodyvanwilderman@gmail.com>
2026-03-08 00:55:31 +0100

    feat: updated 1193

--- 1193.py
+++ 1193.py
@@ -1,74 +1,55 @@
+from collections import deque
+import sys
 n, m = map(int, input().split())
+grid = sys.stdin.readlines()
 
-from queue import Queue
+start = None
+end = None
 
-def parse_path(l, pos_1, pos_2):
-
-    W = len(l[0])
-    H = len(l)
-
-    s = []
-    x = pos_2[0]
-    y = pos_2[1]
-
-    while x != pos_1[0] or y != pos_1[1]:
-        next_pos = l[y][x]
-
-        cx, cy = next_pos[0] - x, next_pos[1] - y
-        if cx == -1:
-            s.append("R")
-        elif cx == 1:
-            s.append("L")
-        elif cy == 1:
-            s.append("U")
-        else:
-            s.append("D")
-
-        x = next_pos[0]
-        y = next_pos[1]
-
-    return "".join(s)[::-1]
-
-def bfs(lab, pos_1, pos_2):
-
-    W = len(lab[0])
-    H = len(lab)
-
-    visited = [[False for _ in range(W)] for _ in range(H)]
-    path = [[(-1, -1) for _ in range(W)] for _ in range(H)]
-
-    queue = Queue()
-    queue.put([pos_1[0], pos_1[1], -1, -1])
-    visited[pos_1[1]][pos_1[0]] = True
-
-    while not queue.empty():
-        x, y, px, py = queue.get()
-        path[y][x] = (px, py)
-        if x == pos_2[0] and y == pos_2[1]:
-            return parse_path(path, pos_1, pos_2)
-
-        for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-            cx = max(0, min(x + dx, W - 1))
-            cy = max(0, min(y + dy, H - 1))
-            if not visited[cy][cx] and lab[cy][cx] != '#' or lab[cy][cx] == "B":
-                queue.put((cx, cy, x, y))
-                visited[cy][cx] = True
+for i in range(n):
+    if "A" in grid[i]:
+        start = i * m + grid[i].index("A")
+    if "B" in grid[i]:
+        end = i * m + grid[i].index("B")
     
-    return ""
+    if start is not None and end is not None:
+        break
 
-lab = [input() for _ in range(n)]
-pos_1 = None
-pos_2 = None
-for j in range(n):
-    if "A" in lab[j]:
-        pos_1 = [lab[j].index("A"), j]
-    if "B" in lab[j]:
-        pos_2 = [lab[j].index("B"), j]
+ancestors = {start:None}
+distances = [float("inf")] * (n * m)
+queue = deque([(start, 0)])
 
-result = bfs(lab, pos_1, pos_2)
-if result != "":
-    print("YES")
-    print(len(result))
-    print(result)
-else:
+while queue:
+    node, dist = queue.pop()
+    if grid[node // m][node % m] == "B":
+        break
+
+    for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+        cx, cy = node % m + dx, node // m + dy
+        if not (0 <= cx < m) or not (0 <= cy < n):
+            continue
+        neigh = cy * m + cx
+        if grid[cy][cx] != "#"and distances[neigh] > dist + 1:
+            ancestors[neigh] = node
+            distances[neigh] = dist + 1
+            queue.appendleft((neigh, dist + 1))
+
+if end not in ancestors:
     print("NO")
+else:
+    print("YES")
+    path = deque()
+    node_from = end
+    while node_from != start:
+        dn = ancestors[node_from] - node_from
+        if dn == -m:
+            path.appendleft("D")
+        elif dn == m:
+            path.appendleft("U")
+        elif dn == -1:
+            path.appendleft("R")
+        else:
+            path.appendleft("L")
+        node_from = ancestors[node_from]
+    print(len(path))
+    print("".join(path))
